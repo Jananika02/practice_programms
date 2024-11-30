de6bdfa015fede0a8690a0e57e9878a5834a8c11
Janani <jananika.perinbanayagam@gmail.com>
2024-11-30 19:27:22 +0530

    print subtract program

---
+++ java/sub.java
@@ -0,0 +1,16 @@
+package practice_programms.java;
+
+import java.util.Scanner;
+
+public class sub {
+    public static void main(String[] agrs) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the Second Number: ");
+        int secondNumber = scanner.nextInt();
+        int result = firstNumber - secondNumber;
+        System.out.println("Enter the number: " + (result));
+        scanner.close();
+    }
+}
