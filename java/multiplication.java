1c71fbbb74e9627068dc05c65241d76b808a96a8
Janani <jananika.perinbanayagam@gmail.com>
2024-11-30 19:40:29 +0530

    print the multiplication program

---
+++ java/multiplication.java
@@ -0,0 +1,16 @@
+package practice_programms.java;
+
+import java.util.Scanner;
+
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the Second Number: ");
+        int secondNumber = scanner.nextInt();
+        int result = (firstNumber * secondNumber);
+        System.out.println("Enter the number: " + (result));
+        scanner.close();
+    }
+}
