0659a2c75d633ec084456491a050c37a9fe77195
Janani <jananika.perinbanayagam@gmail.com>
2024-12-09 20:01:28 +0530

    prime number

---
+++ java/sumofarray.java
@@ -0,0 +1,10 @@
+public class sumofarray {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+            System.out.println("Sum of the array:" + sum);
+        }
+    }
+}
